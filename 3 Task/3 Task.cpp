db1ed6cd9e665a2dfcb6f59c3475c2711c639422
Мария Луговая <lugovaia.mr@students.dvfu.ru>
2020-03-16 16:12:08 +1000

    season tracker

---
+++ 3 Task/3 Task.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "write a month number\n";
+	int month;
+	std::cin >> month;
+
+	if (month < 1) {
+		std::cout << "inwalid month";
+		return 1;
+	}
+	if ((month < 2) || (month == 12))
+		std::cout << "winter";
+	else if (month < 6)
+		std::cout << "spring";
+	else if (month < 9)
+		std::cout << "summer";
+	else if (month < 12)
+		std::cout << "autumn";
+	else
+		std::cout << "inwalid month";
+	return 0;
+}

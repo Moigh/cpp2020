a990250e72f98ee98a0a6b68b85f5a7c90021f98
Мария Луговая <lugovaia.mr@students.dvfu.ru>
2020-03-16 15:51:22 +1000

    amount of nums

--- 2 Задание/2 Задание.cpp
+++ 2 Задание/2 Задание.cpp
@@ -3,20 +3,21 @@
 
 int main()
 {
-    std::cout << "give 5 pos nums\n";
-	int x;
-	int min;
+	int x, n, min;
+	std::cout << "how much nums?\n";
+	std::cin >> n;
+	std::cout << "write pos nums\n";
 	std::cin >> min;
 
-	for (int i = 1; i < 5; ++i) {
+	for (int i = 1; i < n; ++i) {
 		std::cin >> x;
 		if (x < 0) {
-			std::cout << "you wrote neg num\n";
+			std::cout << "neg nums are not allowed\n";
 			return 1;
 		}
 		if (x < min) {
 			min = x;
 		}
 	}
-	std::cout << "your min is " << min;
+	std::cout << "min is " << min;
 }

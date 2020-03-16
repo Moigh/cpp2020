f8d7cc63ca7a322a4530e3ad2911fd0f088fdef6
Мария Луговая <lugovaia.mr@students.dvfu.ru>
2020-03-16 15:45:14 +1000

    min is working

---
+++ 2 Задание/2 Задание.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector> 
+
+int main()
+{
+    std::cout << "give 5 pos nums\n";
+	int x;
+	int min;
+	std::cin >> min;
+
+	for (int i = 1; i < 5; ++i) {
+		std::cin >> x;
+		if (x < 0) {
+			std::cout << "you wrote neg num\n";
+			return 1;
+		}
+		if (x < min) {
+			min = x;
+		}
+	}
+	std::cout << "your min is " << min;
+}
